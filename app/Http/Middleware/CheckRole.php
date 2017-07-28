23a1c62410fdb3121949f769e0667a7faa78fdf7
tsnigucki <tomaszsnigucki@gmail.com>
2017-07-28 14:59:12 +0200

    konfiguracja projektu cz8

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -15,6 +15,17 @@ class CheckRole
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if ($request->user() === null) {
+            return redirect('/login');
+        }
+
+        $actions = $request->route()->getAction();
+        $roles = isset($actions['roles']) ? $actions['roles'] : null;
+
+        if ($request->user()->hasAnyRole($roles)) {
+            return $next($request);
+        }
+
+        return redirect('/login');
     }
 }
